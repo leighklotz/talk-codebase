84d942abb0ecd6f5d250a900c93d92002e2d74cc
Leigh Klotz <klotz@klotz.me>
2023-05-29 13:40:30 -0700

    leighklotz-3: add *.emacs

--- talk_codebase/consts.py
+++ talk_codebase/consts.py
@@ -2,5 +2,5 @@ EXCLUDE_DIRS = ['__pycache__', '.venv', '.git', '.idea', 'venv', 'env', 'node_mo
                 '.github', '.gitlab']
 ALLOW_FILES = ['.txt', '.js', '.mjs', '.ts', '.tsx', '.css', '.scss', '.less', '.html', '.htm', '.json', '.py',
                '.java', '.c', '.cpp', '.cs', '.go', '.php', '.rb', '.rs', '.swift', '.kt', '.scala', '.m', '.h',
-               '.sh', '.pl', '.pm', '.lua', '.sql', '.el', '.ino', '.asm']
+               '.sh', '.pl', '.pm', '.lua', '.sql', '.el', '.emacs', '.ino', '.asm']
 EXCLUDE_FILES = ['requirements.txt', 'package.json', 'package-lock.json', 'yarn.lock']
